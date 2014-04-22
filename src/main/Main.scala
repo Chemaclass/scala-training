c5f3169644e1ec0f7d980dcf565c67588aae0605
José María Valera Reales <chemaclass@outlook.es>
2014-04-22 15:05:08 +0200

    Creo función dentro del main

--- src/main/Main.scala
+++ src/main/Main.scala
@@ -3,7 +3,8 @@ package main
 object Main {
 
 	def main(args: Array[String]) {
-		val m2 = multiplica(2, _: Int)
+		def mul(a: Int, b: Int): Int = return a * b
+		val m2 = mul(2, _: Int)
 		println("2 x 5 = " + m2(5)) // 10
 	}
 
