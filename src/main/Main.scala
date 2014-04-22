741ed2f592a640469c2f35cbc1381b7fb0fae2bf
José María Valera Reales <chemaclass@outlook.es>
2014-04-22 16:21:37 +0200

    introducción a Funciones parciales

--- src/main/Main.scala
+++ src/main/Main.scala
@@ -2,12 +2,26 @@ package main
 
 object Main {
 	def main(args: Array[String]) {
-		Ejemplos ej1 ()
+		Ejemplos ej2 ()
 	}
 }
 
 object Ejemplos {
 
+	// Funciones parciales
+	def ej2() {
+		def conectaServidor: Boolean = { return true }
+		def limpiaEspacio: Boolean = { return false }
+		val traza: PartialFunction[Boolean, String] = {
+			case true => "traza.true"
+		}
+		val error: PartialFunction[Boolean, String] = {
+			case false => "error.false"
+		}
+		val test = traza orElse error
+		println(test(conectaServidor))
+		println(test(limpiaEspacio))
+	}
 	//Crear una func a la mitad
 	def ej1(a: Int = 2, b: Int = 5) {
 		def mul(x: Int, y: Int): Int = return x * y
