aff948f1f654f0048cd5d00db063dadede43c85c
José María Valera Reales <chemaclass@outlook.es>
2014-04-22 15:02:59 +0200

    ignoramos /bin

--- src/main/Main.scala
+++ src/main/Main.scala
@@ -1,7 +1,13 @@
 package main
 
 object Main {
+
 	def main(args: Array[String]) {
-		println("Hello world!")
+		val m2 = multiplica(2, _: Int)
+		println("2 x 5 = " + m2(5)) // 10
+	}
+
+	def multiplica(a: Int, b: Int): Int = {
+		return a * b
 	}
 }
